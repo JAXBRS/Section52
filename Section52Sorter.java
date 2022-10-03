b04a4d979cf35a3b46680315d3d9c92b29ec1c58
alothmanmj <alothman.maj@gmail.com>
2022-10-03 14:24:22 +0300

    add sort method

--- Section52Sorter.java
+++ Section52Sorter.java
@@ -1,5 +1,11 @@
 public class Section52Sorter{
+    static void sort(int[] arr){
+        System.out.println("to be impl");
+    }
     public static void main(String[] args) {
+        int[] arr = {2,1,5,4};
+
         System.out.println("Hello World");
+        sort(arr);
     }
 }
